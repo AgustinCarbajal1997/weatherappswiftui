c45421622d7cd5ce04623aa03236783061055bf7
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-11-12 13:47:01 -0300

    Initial Commit

---
+++ weatherapp/weatherappApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherappApp.swift
+//  weatherapp
+//
+//  Created by Agustin Carbajal on 12/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct weatherappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
